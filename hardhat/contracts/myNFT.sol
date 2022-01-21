3a1e0abbc124a9ebfdffd0f17205a17f60869d15
phdargen <p.dargent@cern.ch>
2022-01-21 18:41:17 +0100

    implemented royalties

--- hardhat/contracts/myNFT.sol
+++ hardhat/contracts/myNFT.sol
@@ -1,21 +1,29 @@
 pragma solidity ^0.8.4;
 //SPDX-License-Identifier: MIT
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/interfaces/IERC2981.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./ColorPalette.sol";
 import "./SVG.sol";
 import "./Helper.sol";
 //import "hardhat/console.sol";
 
-contract myNFT is ERC721, Ownable {
+contract myNFT is ERC721Enumerable, IERC2981, Ownable {
 
   using Strings for uint256;
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
-  uint public constant PRICE = 0.0 ether;
+
+  uint public price = 0.0 ether;
+  uint public maxSupply = 1000;
+  uint public maxPerPerson = 10;
+
+  address public royaltyAddress = address(this);
+  uint public royalty = 1000; // 10%
 
 	ColorPalette colorPaletteSet; 
   SVG svgMaker;
@@ -32,12 +40,14 @@ contract myNFT is ERC721, Ownable {
 
   function mintNFT() public payable returns (uint256)
   {
-      require(msg.value == PRICE, "Wrong price");
+      require(msg.value == price, "Wrong price");
+      require(totalSupply() < maxSupply, "Sold out!");
+      require(balanceOf(msg.sender) + 1 <= maxPerPerson,"You cant mint more than limit");
 
       _tokenIds.increment();
 
       uint256 id = _tokenIds.current();
-      _mint(msg.sender, id);
+      _safeMint(msg.sender, id);
 
       rnd[id] = uint256(keccak256(abi.encodePacked(id, blockhash(block.number-1), msg.sender, address(this)))); 
 
@@ -69,9 +79,9 @@ contract myNFT is ERC721, Ownable {
 
           index += layers;
           circles += figures[index];
-        }
+      }
 
-        string memory attributes = string(
+      string memory attributes = string(
             abi.encodePacked(
                  '", "attributes":[',
                 '{"trait_type": "Color Palette","value":"',
@@ -87,9 +97,9 @@ contract myNFT is ERC721, Ownable {
                 '"}',
                 "]}"
             )
-        );
+      );
 
-        return
+      return
             string(
                 abi.encodePacked(
                     "data:application/json;utf8,",
@@ -107,7 +117,7 @@ contract myNFT is ERC721, Ownable {
                         )
                     )
                 )
-            );
+      );
   }
 
   function getSVG(uint256 id) public view returns (string memory) {
@@ -118,4 +128,49 @@ contract myNFT is ERC721, Ownable {
     return svgMaker.getSVG(rnd[id], colorPalette);
   }
 
+  /// @dev Support for IERC-2981, royalties
+  function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721Enumerable, IERC165) returns (bool) {
+        return interfaceId == type(IERC2981).interfaceId || super.supportsInterface(interfaceId);
+  }
+
+  function royaltyInfo(uint256, uint256 _salePrice)
+        public
+        view
+        override
+        returns (address, uint256)
+  {
+        return (royaltyAddress, (_salePrice * royalty) / 10000);
+  }
+
+  function setPrice(uint256 newPrice) public onlyOwner {
+        price = newPrice;
+  }
+
+  function setRoyalty(uint16 _royalty) external onlyOwner {
+        require(_royalty >= 0, "Royalty must be >= 0%");
+        require(_royalty <= 1000, "Royalty must be <= 10%" );
+        royalty = _royalty;
+  }
+    
+  function setRoyaltyAddress(address _royaltyAddress) external onlyOwner {
+        royaltyAddress = _royaltyAddress;
+  }
+
+  // Withdraw from contract
+  function withdraw() external onlyOwner {
+        uint balance = address(this).balance;
+        payable(msg.sender).transfer(balance);
+  }
+
+  // Withdraw any ERC20 token
+  function withdrawERC20(address token) external onlyOwner {
+        IERC20(token).transfer(owner(), IERC20(token).balanceOf(address(this)));
+  }
+
+  // Withdraw any ERC721 token
+  function withdrawERC721(address token, uint256 tokenId) external onlyOwner {
+        IERC721(token).transferFrom(address(this), owner(), tokenId);
+  }
+
+  receive() external payable {}
 }

44fe08e105fca5c2fe0268d42215b9d5634ff7db
Philippe d'Argent <p.dargent@cern.ch>
2024-04-03 19:02:16 +0200

    added home for fframe

--- farcaster_frame/app/page.tsx
+++ farcaster_frame/app/page.tsx
@@ -1,8 +1,9 @@
-import { getFrameMetadata } from 'frog/next'
-import type { Metadata } from 'next'
-import Image from 'next/image'
+// Import React if you need to use JSX
+import React from 'react';
+import { getFrameMetadata } from 'frog/next';
+import type { Metadata } from 'next';
 
-import styles from './page.module.css'
+const backgroundImageUrl = 'https://on-chain-art-theta.vercel.app/outPaths.gif';
 
 export async function generateMetadata(): Promise<Metadata> {
   const frameTags = await getFrameMetadata(
@@ -14,11 +15,33 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default function Home() {
+  // Inline style for the main tag
+  const mainStyle = {
+    backgroundImage: `url(${backgroundImageUrl})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  };
+
+  // Inline style for the button
+  const buttonStyle = {
+    padding: '10px 20px',
+    fontSize: '32px',
+    color: 'white',
+    backgroundColor: '#007bff',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-      XONIN
-      </div>
+    <main style={mainStyle}>
+      <a href="http://onchainart.s3-website.us-east-2.amazonaws.com/" style={{ textDecoration: 'none' }}>
+        <button style={buttonStyle}>Mint</button>
+      </a>
     </main>
-  )
+  );
 }
